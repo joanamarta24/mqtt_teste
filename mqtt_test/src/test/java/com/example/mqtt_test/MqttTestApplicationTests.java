0d8cac7f1f48e875aaa9bad7c080f948ac4c9e5a
joanamarta <joanamartasouza@gmail.com>
2025-11-07 16:52:22 -0300

    07/11/2025

---
+++ mqtt_test/src/test/java/com/example/mqtt_test/MqttTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mqtt_test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MqttTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
